ba8e9e4d26b0a367ddc2e74e03bcaf82812d7d38
Cristhian Silveira <cristhianmantenha60@gmail.com>
2023-11-13 12:46:57 -0300

    React Carro

---
+++ React/resumo-react/src/components/Carro.js
@@ -0,0 +1,9 @@
+function Carro({cor, marca, modelo}) {
+    return(
+        <div>
+            <p>Olá, Eu tenho um Carro {modelo} {cor} da marca {marca}!</p>
+        </div>
+    )
+}
+
+export default Carro
