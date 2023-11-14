16904e07ddd387513bb64de7ef30f5252b6f14d7
Cristhian Silveira <cristhianmantenha60@gmail.com>
2023-11-14 18:33:28 -0300

    React Fragment

---
+++ React/resumo-react/src/components/Fragments.js
@@ -0,0 +1,10 @@
+function Fragments() {
+    return(
+        <>
+            <p>Eu não tenho elemento pai e uso fragment</p>
+            <p>Abre o console pra ver que não tenho pai(div)</p>
+        </>
+    )
+}
+
+export default Fragments
